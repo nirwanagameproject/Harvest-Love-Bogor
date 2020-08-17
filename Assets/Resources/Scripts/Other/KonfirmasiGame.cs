7dee907a206a9b3c8c63dd5df604ebf43105ca7b
Fajar Muhammad Fathirochman <fajarmuhf@gmail.com>
2020-08-17 23:05:56 +0700

    Perbaiki bugs konfirmasi
    perbaiki konfirmasi button menghilang

--- Assets/Resources/Scripts/Other/KonfirmasiGame.cs
+++ Assets/Resources/Scripts/Other/KonfirmasiGame.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class KonfirmasiNewGame : MonoBehaviour
+public class KonfirmasiGame : ChangeLanguage
 {
     // Start is called before the first frame update
     void Start()
