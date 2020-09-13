92aa11270321e73ad28b0b76e87ba28ffcd88701
Fajar Muhammad Fathirochman <fajarmuhf@gmail.com>
2020-09-13 21:12:23 +0700

    Update kotakperalatan.cs

--- Assets/Resources/Scripts/Gameplay/kotakperalatan.cs
+++ Assets/Resources/Scripts/Gameplay/kotakperalatan.cs
@@ -162,7 +162,7 @@ public class kotakperalatan : MonoBehaviour
 
             if ((newtools.slot == 0 && sebelumtools.name!="") || (sebelumtools.slot == 0 && newtools.name != ""))
             {
-                GameObject myweapon = Gamesetupcontroller.instance.go.transform.Find("Root").Find("J_Bip_C_Hips").Find("J_Bip_C_Spine").Find("J_Bip_C_Chest").Find("J_Bip_C_UpperChest").Find("J_Bip_R_Shoulder").Find("J_Bip_R_UpperArm").Find("J_Bip_R_LowerArm").Find("J_Bip_R_Hand").Find("weapon").gameObject;
+                GameObject myweapon = Gamesetupcontroller.instance.go.transform.Find("Armature").Find("Hips").Find("Spine").Find("Chest").Find("Right shoulder").Find("Right arm").Find("Right elbow").Find("Right wrist").Find("weapon").gameObject;
                 for (int i = 0; i < myweapon.transform.childCount; i++)
                     myweapon.transform.GetChild(i).gameObject.SetActive(false);
                 myweapon.transform.Find(PlayerPrefs.GetString("peralatannama0")).gameObject.SetActive(true);
@@ -177,7 +177,7 @@ public class kotakperalatan : MonoBehaviour
             } 
             else if ((newtools.slot == 0 && sebelumtools.name == "") || (sebelumtools.slot == 0 && newtools.name == ""))
             {
-                GameObject myweapon = Gamesetupcontroller.instance.go.transform.Find("Root").Find("J_Bip_C_Hips").Find("J_Bip_C_Spine").Find("J_Bip_C_Chest").Find("J_Bip_C_UpperChest").Find("J_Bip_R_Shoulder").Find("J_Bip_R_UpperArm").Find("J_Bip_R_LowerArm").Find("J_Bip_R_Hand").Find("weapon").gameObject;
+                GameObject myweapon = Gamesetupcontroller.instance.go.transform.Find("Armature").Find("Hips").Find("Spine").Find("Chest").Find("Right shoulder").Find("Right arm").Find("Right elbow").Find("Right wrist").Find("weapon").gameObject;
                 for (int i = 0; i < myweapon.transform.childCount; i++)
                     myweapon.transform.GetChild(i).gameObject.SetActive(false);
 
