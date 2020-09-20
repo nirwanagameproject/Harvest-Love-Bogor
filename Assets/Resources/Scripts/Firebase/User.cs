04c2610541c984b9cc3479aabb57886626529638
fandymuhf <fandy.zea@gmail.com>
2020-09-21 01:02:54 +0700

    Add Firebase signin db

---
+++ Assets/Resources/Scripts/Firebase/User.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using Firebase;
+using Firebase.Unity.Editor;
+using Firebase.Database;
+
+[Serializable]
+public class User
+{
+    public string id;
+    public LinkedAcc linkedacc;
+
+    public User(string id, LinkedAcc linkedAcc)
+    {
+        this.id = id;
+        this.linkedacc = linkedAcc;
+    }
+}
+
+[Serializable]
+public class LinkedAcc
+{
+    public string tipeacc;
+    public string email;
+    public string name;
+    public string id;
+    public LinkedAcc(string tipeacc, string email, string name, string id)
+    {
+        this.tipeacc = tipeacc;
+        this.email = email;
+        this.name = name;
+        this.id = id;
+    }
+}
+
